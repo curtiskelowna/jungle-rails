b7d2b6985cb852d854732f5568b68234270a2d65
Curtis Platt <curtisplatt@hotmail.com>
2023-09-26 14:03:37 -0400

    user authentication feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
     get 'categories/index'
